f69d9f1bd68ec9fd8f0653abf0782543ce4d6248
Jorge Murillo <jorge.murillo0774@gmail.com>
2018-02-19 17:07:20 -0500

    Fix JS syntax error

--- js/custom.js
+++ js/custom.js
@@ -154,9 +154,9 @@ $.fn.isInViewport = function() {
 
 $(window).on('resize scroll', function() {
   $('.main').each(function() {
-  	var className = $(this).attr('id')
+  	var className = $(this).attr('id');
     if ($(this).isInViewport()) {
-  	  console.log("Currently on + " className)
+  	  console.log("Currently on + " className);
       $("#main-" + className).toggleClass('active');
     } else {
       $("#main-" + className).removeClass('active');
